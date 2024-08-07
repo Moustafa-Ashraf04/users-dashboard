e7e06fe1f306e8a0e82c5eeb45fd0997f9976164
Moustafa_Ashraf <moustafa.ashraf277@gmail.com>
2024-08-07 23:59:24 +0300

    feat : add display users page

--- src/app/display-users/display-users.component.ts
+++ src/app/display-users/display-users.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../header/header.component';
+import { UserCardComponent } from '../user-card/user-card.component';
+import { PaginationComponent } from "../pagination/pagination.component";
 
 @Component({
   selector: 'app-display-users',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserCardComponent, PaginationComponent],
   templateUrl: './display-users.component.html',
-  styleUrl: './display-users.component.css'
+  styleUrl: './display-users.component.css',
 })
 export class DisplayUsersComponent {
-
+  items = [1, 2, 3, 4, 5, 6];
 }
